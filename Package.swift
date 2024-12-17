9fdf1c0c2915a6ea516f419c324280da7ac8845d
Matthew Reed <mattreed1890@gmail.com>
2024-12-16 19:23:54 -0600

    update markymark

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "StaticShock",
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
-        .package(url: "https://github.com/matty316/markymark.git", exact: "0.0.2"),
+        .package(url: "https://github.com/matty316/markymark.git", exact: "0.0.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
